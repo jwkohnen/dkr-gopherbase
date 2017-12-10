f59663d20b8e6061940e9835c299cd96b01c01df
Johannes Kohnen <wjkohnen@users.noreply.github.com>
2017-12-10 13:15:44 +0100

    Use simple build script for image LABELing

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -xeuo pipefail
+
+VERSION=$(git describe --tags --always --dirty="-dev")
+BUILD_DATE=$(date -u '+%Y-%m-%d-%H%M UTC')
+VCS_REF=$(git rev-parse HEAD)
+docker build . -f Dockerfile -t wjkohnen/gopherbase:${TAG} \
+	--build-arg VERSION="${VERSION}" \
+	--build-arg BUILD_DATE="${BUILD_DATE}" \
+	--build-arg VCS_REF="${VCS_REF}" 
